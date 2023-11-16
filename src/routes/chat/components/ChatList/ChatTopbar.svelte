b3619aefb0af443a49cdd4edd71f1fd55abed982
Gedeon Lenz <email@gedeonlenz.com>
2023-11-16 11:56:07 -0800

    added stores

--- src/routes/chat/components/ChatList/ChatTopbar.svelte
+++ src/routes/chat/components/ChatList/ChatTopbar.svelte
@@ -1,15 +1,14 @@
 <script lang="ts">
     import {type User, UserRole} from "$lib/types.js";
     import {newChatVisible} from "$lib/chat/states";
-
-    export let currentUser:User;
+    import {currentUser} from "$lib/chat/user";
 </script>
 
 <div class="chats-topbar">
     <h2 class="chats-heading">Chats</h2>
     <div class="topbar-button-wrapper">
         <a href="/logout"><i class="fa-solid fa-arrow-right-from-bracket"></i></a>
-        {#if currentUser.type === UserRole.ServiceProvider}
+        {#if $currentUser !== undefined && $currentUser.type === UserRole.ServiceProvider}
             <i id="newchat" role="button"  tabindex="0" on:keydown={() => {newChatVisible.set(true);}} on:click={() => {newChatVisible.set(true);}} class="fa-regular fa-pen-to-square"></i>
         {/if}
     </div>
