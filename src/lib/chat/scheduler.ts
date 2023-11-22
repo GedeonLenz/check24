8af459f2f381055d5d33394a05774656b1c2463d
Gedeon Lenz <email@gedeonlenz.com>
2023-11-21 18:14:29 -0800

    undo

--- src/lib/chat/scheduler.ts
+++ src/lib/chat/scheduler.ts
@@ -1,7 +1,6 @@
-import {get, writable, type Writable} from "svelte/store";
+import {writable, type Writable} from "svelte/store";
 import {updateSelectedConversation} from "$lib/chat/conversations";
 import {fetchCurrentMessages} from "$lib/chat/messages";
-import {currentUser} from "$lib/chat/user";
 
 export const schedulaerChatList:Writable<NodeJS.Timeout | null>  = writable(null);
 export const schedulaerChatMessages:Writable<NodeJS.Timeout | null>  = writable(null);
@@ -14,9 +13,6 @@ export async function startAllSchedulers() {
 const delay = 5*1000;
 export async function startChatListUpdater() {
     let scheduler = setInterval(async () => {
-        if(get(currentUser) === undefined) {
-            return;
-        }
         await updateSelectedConversation();
     }, delay);
     schedulaerChatList.set(scheduler);
@@ -24,9 +20,6 @@ export async function startChatListUpdater() {
 
 export async function startChatMessageUpdater() {
     let scheduler = setInterval(async () => {
-        if(get(currentUser) === undefined) {
-            return;
-        }
         await fetchCurrentMessages(true);
     }, delay);
     schedulaerChatMessages.set(scheduler);
